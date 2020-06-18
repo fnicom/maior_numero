fb56492d6977a84b99ab58080fc1fcb678c2f82c
Francisco Nicomedes <52546292+fnicom@users.noreply.github.com>
2020-06-17 21:25:11 -0300

    Convertendo String em Number

---
+++ maior.js
@@ -0,0 +1,9 @@
+let numero1 = Number (window.prompt('Digite o numero 1'));
+let numero2 = Number (window.prompt('Digite o numero 2'));
+
+if (numero1 > numero2) {
+    alert(numero1)
+}else{
+    alert(numero2)
+}
+
